bb3cd174e223bfc875ecd77478cc279a12c20d2a
ashish jain <ashish.jain8847@gmail.com>
2018-08-09 14:20:14 +0530

    First Commit to the chat app

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const {generateMessage} = require('./utils/message');
+
+const publicPath = path.join(__dirname, '../public'); 
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app); 
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+console.log('New User connected');
+
+//socket.emit to user who join from admin
+socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat application'));
+//socket.broadcast.emit from text new user joined
+socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+
+socket.on('createMessage', (message) => {
+console.log('createMessage', message);
+io.emit('newMessage', generateMessage(message.from, message.text));
+});
+
+socket.on('disconnect', () => {
+    console.log('User was disconnected');
+
+});
+});
+
+server.listen(port, () =>  {
+console.log(`Server is started on port ${port}`);
+});
