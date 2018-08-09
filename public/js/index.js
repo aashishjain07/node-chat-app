e3191085a3e78d08b2474c8c9cbe48093e87963a
ashish jain <ashish.jain8847@gmail.com>
2018-08-09 15:16:04 +0530

    commiting for heroku

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 var socket = io();
-
+// creating events for index.html file
 socket.on('connect', function() {
 console.log('Connected to Server');
 });
+
 socket.on('disconnect', function() {
-    console.log('Disconnected from server');
+console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function(message) {
